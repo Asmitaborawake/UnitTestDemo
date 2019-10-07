826802d91e9bb950c5ff69c63f266938a097d9dd
Asmita Borawake <asmita.borawake@Sneha-Sures-Mac.local>
2019-10-07 17:44:35 +0530

    unit test demo

---
+++ UniteTestDemo/Addition.swift
@@ -0,0 +1,9 @@
+//
+//  Addition.swift
+//  UniteTestDemo
+//
+//  Created by Asmita Borawake on 07/10/19.
+//  Copyright © 2019 Asmita Borawake. All rights reserved.
+//
+
+import Foundation
---
+++ UniteTestDemo/MaxNumber.swift
@@ -0,0 +1,9 @@
+//
+//  MaxNumber.swift
+//  UniteTestDemo
+//
+//  Created by Asmita Borawake on 07/10/19.
+//  Copyright © 2019 Asmita Borawake. All rights reserved.
+//
+
+import Foundation
